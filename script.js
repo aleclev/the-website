fa7f98aa5f4e77b9e666b5bc9d0df556e41973e7
aleclev <alec.lev11@gmail.com>
2024-11-22 00:37:41 -0500

    added some weird cat fact api

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 let counterValue = 0;
 let buttonElement;
+let randomCatFactElement;
 
 function init() {
 	buttonElement = document.getElementById("the-button");
+	randomCatFactElement = document.getElementById("random-cat-fact");
+	inject_cat_fact();
 }
 
 function click_count() {
 	counterValue++;
 	buttonElement.innerHTML = counterValue;
+}
+
+function inject_cat_fact() {
+	fetch("https://cat-fact.herokuapp.com/facts/random").then(
+		(res) => {
+			if (res.ok) {
+				res.json().then((re) => {
+					randomCatFactElement.innerHTML = re.text;
+			
+				});
+			}
+		}
+	);
 }
